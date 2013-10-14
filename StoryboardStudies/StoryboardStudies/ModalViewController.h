5fda92968ab1da08d57caf30487fbdbce63c41da
Natsuwind <adminywen@DELL-MPC.local>
2013-10-14 13:33:44 +0800

    add modal view

---
+++ StoryboardStudies/StoryboardStudies/ModalViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModalViewController.h
+//  StoryboardStudies
+//
+//  Created by adminywen on 13-10-14.
+//  Copyright (c) 2013年 adminywen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalViewController : UIViewController
+- (IBAction)dismiss:(id)sender;
+@end
---
+++ StoryboardStudies/StoryboardStudies/ModalViewController.m
@@ -0,0 +1,43 @@
+//
+//  ModalViewController.m
+//  StoryboardStudies
+//
+//  Created by adminywen on 13-10-14.
+//  Copyright (c) 2013年 adminywen. All rights reserved.
+//
+
+#import "ModalViewController.h"
+
+@interface ModalViewController ()
+
+@end
+
+@implementation ModalViewController
+
+-(IBAction)dismiss:(id)sender
+{
+    [[self presentingViewController] dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
