43cc57c065777b664e0349ab13840062bc96d558
Natsuwind <adminywen@DELL-MPC.local>
2013-10-14 12:58:06 +0800

    new storyboard project

---
+++ StoryboardStudies/StoryboardStudies/StoryStuAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  StoryStuAppDelegate.h
+//  StoryboardStudies
+//
+//  Created by adminywen on 13-10-14.
+//  Copyright (c) 2013年 adminywen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoryStuAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StoryboardStudies/StoryboardStudies/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoryboardStudies
+//
+//  Created by adminywen on 13-10-14.
+//  Copyright (c) 2013年 adminywen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "StoryStuAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([StoryStuAppDelegate class]));
+    }
+}
